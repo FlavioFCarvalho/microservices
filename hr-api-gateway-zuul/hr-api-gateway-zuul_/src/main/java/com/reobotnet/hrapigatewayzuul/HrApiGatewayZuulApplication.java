8fdc45f150a1b8b019dd43b31e6be2b8f0a53352
FlavioFCarvalho <flaviocfr@gmail.com>
2021-11-04 19:25:40 -0300

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/hr-api-gateway-zuul_/src/main/java/com/reobotnet/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/hr-api-gateway-zuul_/src/main/java/com/reobotnet/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.reobotnet.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
@@ -11,3 +15,4 @@ public class HrApiGatewayZuulApplication {
 	}
 
 }
+
